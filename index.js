205649a8884f6585e207a2a17d24835390770710
phthallo <84078890+phthallo@users.noreply.github.com>
2024-06-21 23:53:01 +0930

    feat: support for multiple terminal messages :D
    - remove caret :(

--- index.js
+++ index.js
@@ -1,61 +1,42 @@
-
-
 document.addEventListener("keydown", keyDownTextField, false);
-document.addEventListener("click", checkCaretLocation, false);
-var textEditable = true;
-
-function caretState(state) {
-    console.log(`Caret visibility was set to ${state}!`)
-    document.getElementById("caret").hidden = !state;
-}
-
-function checkCaretLocation(){
-    if (document.activeElement.isContentEditable) {
-        caretState(false);
-        console.log("Click was inside editable text field!");
-    } else if (textEditable) {
-        caretState(true);
-        console.log("Click was outside editable text field!");
-    }
-}
+var consol = document.getElementById("console");
 
 function keyDownTextField(e) {
     var keyCode = e.keyCode;
-    if(keyCode==13 && document.activeElement.closest("[contenteditable]")) {
-        const textField = document.querySelector("#text-input");
+    if(keyCode==13 && document.activeElement.isContentEditable) {
+        const textField = document.activeElement;
         textField.setAttribute("contenteditable", false);
-        textEditable = false;
         e.preventDefault();
-        caretState(false);
         parseTextInput(textField.innerText);
         console.log("Enter key was pressed while cursor was inside an editable text field!");
     }
-    if(keyCode==13 & !(document.activeElement.closest("[contenteditable]"))){
+    if(keyCode==13 & !(document.activeElement.isContentEditable)){
         console.log("Enter key was pressed when cursor was not inside an editable text field!")
     }
 }
-
 function parseTextInput(text){
     console.log(text);
     [command, parameter] = text.split(" ");
     console.log(`Command is ${command}`)
     console.log(`Parameter is ${parameter}`)
-    cat(`files/${parameter}.html`);
+    if (command=="cat"){
+        cat(parameter);
+    } else {
+        consol.innerHTML += `<div id = "">-bash: ${text}: command not found</div><br>`;
+    }
+    cat("load_text");
+
 }
 
 function cat(file){
-    fetch(file)
-        .then((res) => res.text())
-        .then((text) => {
-            var console = document.getElementById("console");
-            console.innerHTML += text;
-        })
-        .catch((e) => console.error(e));
-    fetch("files/load_text.html")
-        .then((res) => res.text())
-        .then((text) => {
-            var console = document.getElementById("console");
-            console.innerHTML += text;
-        })
-        .catch((e) => console.error(e));
+    if (!(["about_me", "load_text", "projects"].includes(file))){
+        consol.innerHTML += `<div id = "">cat: ${file}: No such file or directory</div><br>`;
+    } else {
+        fetch(`files/${file}.html`)
+            .then((res => res.text()))
+            .then((text) => {
+                consol.innerHTML += ('<div id = "">' + text + '</div><br>');
+            });
+    }
+
 }
