73ecb23a05e69357d8eaa3261c5f6ed3ad695ad4
phthallo <84078890+phthallo@users.noreply.github.com>
2024-06-22 18:42:10 +0930

    feat: message timestamps
    - add message timestamps (hh:mm:ss)
    - add auto cursor focus for new messages

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 document.addEventListener("keydown", keyDownTextField, false);
-document.addEventListener("keydown", focusText, false);
-document.addEventListener("onload", focusText, false);
+document.addEventListener("keyup", focusText, false);
 
 var consol = document.getElementById("console");
 
-function focusText(e) {
-    var text = consol.querySelector("div:last-child .text-input");
-    console.log(text);
-    text.focus();
+function genTimestamp() {
+    currentTime = new Date();
+    hours = String(currentTime.getHours()).padStart(2, '0');
+    minutes = String(currentTime.getMinutes()).padStart(2, '0');
+    seconds = String(currentTime.getSeconds()).padStart(2, '0');
+    return (`(${hours}:${minutes}:${seconds})`)
 }
+
+consol.querySelector("#l1 .console-input").innerHTML = `
+        <b>phthallo@hackclub.app ${genTimestamp()}</b>:~$
+        <span class = "text-input"  spellcheck="false" contenteditable = "false">ls</span>
+`
+
+consol.querySelector("#l2 .console-input").innerHTML = `
+        <b>phthallo@hackclub.app ${genTimestamp()}</b>:~$
+        <span class = "text-input"  spellcheck="false" contenteditable = "true"></span>
+`
+
+function focusText(e) {
+    var keyCode = e.keyCode;
+    if(keyCode==13){
+        var text = consol.querySelector("div:last-child .text-input");
+        console.log(text);
+        text.focus();
+    }
+}
+
 
 function keyDownTextField(e) {
     var keyCode = e.keyCode;
