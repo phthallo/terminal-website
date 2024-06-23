5ef243d543bfbedaf5808842f0ba469842e8c9fd
phthallo <84078890+phthallo@users.noreply.github.com>
2024-06-23 22:26:39 +0930

    track files
    i keep forgetting to git add files!! uhjfsdkfj

---
+++ scripts/utils.js
@@ -0,0 +1,23 @@
+export function sanitise(string) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        "/": '&#x2F;',
+    };
+    const reg = /[&<>"'/]/ig;
+    return string.replace(reg, (match)=>(map[match]));
+  }
+  
+export function onpaste(e){
+    e.preventDefault();
+    var text = "";
+    if (e.clipboardData && e.clipboardData.getData) {
+      text = e.clipboardData.getData("text/plain");
+    } else if (window.clipboardData && window.clipboardData.getData) {
+      text = window.clipboardData.getData("Text");
+    }
+    document.execCommand("insertHTML", false, text);
+}
