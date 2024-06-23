037b7a565c2ce67c9b63ebc964bbe11153a35f13
phthallo <84078890+phthallo@users.noreply.github.com>
2024-06-23 12:02:14 +0930

    feat: hyfetch
    waow pride month

---
+++ utils.js
@@ -0,0 +1,46 @@
+function hyfetch(distroAscii, flagColours){
+    colouredLines = []
+    lines = distroAscii.split("\n"); // .length = the amount of lines the distroAscii is
+    console.log(lines);
+    lengthofEach = Math.floor(lines.length/flagColours.length);
+    for (const i in flagColours){
+            for (const j in (lines.slice(lengthofEach*(i-1), i*lengthofEach))){
+                nextLine = (lines.slice(lengthofEach*(i-1), i*lengthofEach)[j]);
+                colouredLines.push(`<pre style = "color: ${flagColours[i]}">${nextLine}</pre>`);
+            }
+        }
+    difference  = lines.length - (lengthofEach*flagColours.length);
+    console.log(difference);
+    for (let i = (difference+1); i >= 0; i--){
+        console.log(lines[lines.length-1-i]);
+        colouredLines.push(`<pre style = "color: ${flagColours[flagColours.length-1]}">${lines[lines.length-1-i]}</pre>`);
+    }
+    return colouredLines;
+    }
+
+colouredAscii = (hyfetch(`
+          _,met$$$$$gg.              
+       ,g$$$$$$$$$$$$$$$P.           
+     ,g$$P"        "\""Y$$.".        
+    ,$$P'              \`$$$.         
+   ',$$P       ,ggs.     \`$$b:       
+   \`d$$'     ,$P"'   .    $$$        
+    $$P      d$'     ,    $$P        
+    $$:      $$.   -    ,d$$'        
+    $$;      Y$b._   _,d$P'          
+    Y$$.    \`.\`"Y$$$$P"'             
+    \`$$b      "-.__
+     \`Y$$                            
+      \`Y$$.
+        \`$$b.
+          \`Y$$b.
+             \`"Y$b._
+                 \`"\""
+    `, [ "#b63c20",
+                "#e69f70", "#FFFFFF", "#c275a3", "#8b185d"]))
+
+console.log(colouredAscii.length);
+
+for (const i in colouredAscii){
+    document.querySelector("#distro").innerHTML += colouredAscii[i];
+}
