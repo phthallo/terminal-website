718dbe21cb3f8fc89419946e379e879e48b74b7a
phthallo <84078890+phthallo@users.noreply.github.com>
2024-07-03 19:03:35 +0930

    fix: first flag colour of hyfetch not appearing

--- scripts/hyfetch.js
+++ scripts/hyfetch.js
@@ -2,14 +2,14 @@ function hyfetch(distroAscii, flagColours){
     colouredLines = []
     lines = distroAscii.split("\n"); // .length = the amount of lines the distroAscii is
     lengthofEach = Math.floor(lines.length/flagColours.length);
-    for (const i in flagColours){
+    for (let i = 0; i < flagColours.length; i++){
             for (const j in (lines.slice(lengthofEach*(i-1), i*lengthofEach))){
                 nextLine = (lines.slice(lengthofEach*(i-1), i*lengthofEach)[j]);
-                colouredLines.push(`<pre style = "color: ${flagColours[i]}">${nextLine}</pre>`);
+                colouredLines.push(`<pre style = "color: ${flagColours[i-1]}">${nextLine}</pre>`);
             }
         }
     difference  = lines.length - (lengthofEach*flagColours.length);
-    for (let i = (difference+1); i >= 0; i--){
+    for (let i = (difference+2); i >= 0; i--){
         colouredLines.push(`<pre style = "color: ${flagColours[flagColours.length-1]}">${lines[lines.length-1-i]}</pre>`);
     }
     return colouredLines;
@@ -33,8 +33,7 @@ colouredAscii = (hyfetch(`
           \`Y$$b.
              \`"Y$b._
                  \`"\""
-    `, [ "#915346",
-                "#c8a58d", "#FFFFFF", "#ad8a9f", "#6e3557"]))
+    `, ["#915346", "#c8a58d", "#FFFFFF", "#ad8a9f", "#6e3557"]))
 
 for (const i in colouredAscii){
     document.querySelector("#distro").innerHTML += colouredAscii[i];
