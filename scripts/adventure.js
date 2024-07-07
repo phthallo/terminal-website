6884d44160db7a20aa9ea6fec08dcace9eb67ade
phthallo <84078890+phthallo@users.noreply.github.com>
2024-07-07 16:57:53 +0930

    feat: finish text adventure game!

--- scripts/adventure.js
+++ scripts/adventure.js
@@ -1,4 +1,5 @@
 var currentRoom = "outside"
+var inventory = []
 console.log(`
             _.--._
             _|__|_
@@ -35,25 +36,35 @@ A TARDIS has materialised!
 Do you want to play a game? (yes())`)
 
 function yes(){
-    console.log("What do you want to do next? go (inside()/outside())")
+    console.log("What do you want to do next? (explore() / exit())")
 }
 
-function inside(){
+function explore(){
     switch(currentRoom){
         case("outside"):
             console.log("You open the door. It's dark inside, but there's a faint light in the middle.")
-            console.log("What do you want to do next? look (closer()/outside())")
+            console.log("What do you want to do next? (closer())")
             currentRoom = "consoleRoom"
             break;
         case("TARDISMain"):
             console.log("You cautiously approach one of the entrances in the room. As you enter, the lights turn on.")
-            console.log("What do you want to do next? look (inside())")
+            console.log("What do you want to do next? (explore())")
             currentRoom = "TARDISHallway"
             break;
         case("TARDISHallway"):
             console.log("The lights pulse gently as you pass through the entrance of the hallway. You can see two doors to your right and left.")
-            console.log("What do you want to do next? try the door on the (left() / right())")
+            console.log("What do you want to do next? try the door on the (left() / right()) / exit())")
             break;
+        case("TARDISLibrary"):
+            console.log("You enter the room.")
+            if (!(inventory.includes("keycard"))){
+                console.log("There's a keycard on the middle table. You put it in your pocket.")
+                inventory.push("keycard")
+            }
+            console.log("It's quite dark. The shadows make you feel uneasy.")
+            console.log("What do you want to do next? exit()")
+
+
     }
 }
 
@@ -61,17 +72,33 @@ function left(){
     switch(currentRoom){
         case("TARDISHallway"):
             console.log("It's locked.")
-            inside()
+            explore()
             break;
     }
 }
 
-function outside(){
+function right(){
     switch(currentRoom){
-        case("consoleRoom"):
+        case("TARDISHallway"):
+            console.log("The door slides open. It looks like a ... library?")
+            console.log("What do you want to do next? (explore() / exit())")
+            currentRoom = "TARDISLibrary"
+    }
+}
+
+function exit(){
+    switch(currentRoom){
         case("outside"):
             console.log("You look away from the TARDIS. When you look back, it's gone.")
             currentRoom = "gameOver"
+            break;
+        case("TARDISHallway"):
+            currentRoom = "consoleRoom"
+            closer()
+            break;
+        case("TARDISLibrary"):
+            currentRoom = "TARDISHallway"
+            explore()
             break;
 }
 }
@@ -79,8 +106,8 @@ function outside(){
 function closer(){
     switch(currentRoom){
         case("consoleRoom"):
-            console.log("As you approach the console, the lights flash. Suddenly, the door shuts behind you! ")
-            console.log("What do you want to do next? look (inside()) / inspect()")
+            console.log("As you approach the console, the lights flash.")
+            console.log("What do you want to do next? (explore()) / inspect())")
             currentRoom = "TARDISMain"
             break;
     }
@@ -91,5 +118,36 @@ function inspect(){
         case("TARDISMain"):
             console.log("You look carefully at the controls in front of you. Two things stand out: a lever and a button.")
             console.log("What do you want to do next? use (lever()) / button())")
+            break;
+            }
+}
+
+function button(){
+    switch(currentRoom){
+        case("TARDISMain"):
+            if (!(inventory.includes("keycard"))){
+                console.log("You try to press the button, but nothing happens.")
+                console.log("A screen nearby flashes with a message: PERMISSION DENIED.")
+                currentRoom = "consoleRoom"
+                closer()
+                break;
+
+            } else {
+                console.log("You press the button and hear a clicking noise.")
+                console.log("The front door swings open! Grateful to be free, you walk through.")
+                currentRoom = "outside"
+                exit()
+                break;
+            }
+    }
+}
+
+function lever(){
+    switch(currentRoom){
+        case("TARDISMain"):
+            console.log("You pull the lever. There's an ominious hissing noise, and then everything goes black.")
+            currentRoom = "gameOver"
+            break;
+
     }
 }
