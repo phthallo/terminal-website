367a814b0ed4c1672f437cd867f36d482b61d1a0
phthallo <annabelquach13@gmail.com>
2024-10-17 20:11:10 +1030

    fix: console game - add extra text for endings - fix undefined return value of functions - format options better

--- scripts/adventure.js
+++ scripts/adventure.js
@@ -1,153 +1,170 @@
-var currentRoom = "outside"
-var inventory = []
+var currentRoom = "outside";
+var inventory = [];
+var info;
+var steps = 0;
+var libraryVisited = 0;
 console.log(`
-            _.--._
-            _|__|_
-_____________|__|_____________
-.-'______________________________'-.
-| |________POLICE___BOX__________| |
-|  |============================|  |
-|  | .-----------..-----------. |  |
-|  | |  _  _  _  ||  _  _  _  | |  |
-|  | | | || || | || | || || | | |  |
-|  | | |_||_||_| || |_||_||_| | |  |
-|  | | | || || | || | || || | | |  |
-|  | | |_||_||_| || |_||_||_| | |  |
-|  | |  _______  ||  _______  | |  |
-|  | | |       | || |       | | |  |
-|  | | |       | || |       | | |  |
-|  | | |       | || |       | | |  |
-|  | | |_______| || |_______| | |  |
-|  | |  _______ @||@ _______  | |  |
-|  | | |       | || |       | | |  |
-|  | | |       | || |       | | |  |
-|  | | |       | || |       | | |  |
-|  | | |_______| || |_______| | |  |
-|  | |  _______  ||  _______  | |  |
-|  | | |       | || |       | | |  |
-|  | | |       | || |       | | |  |
-|  | | |       | || |       | | |  |
-|  | | |_______| || |_______| | |  |
-|  | '-----------''-----------' |  |
-_|__|/__________________________\|__|_ 
+                 _.--._
+                 _|__|_
+     _____________|__|_____________
+  .-'______________________________'-.
+  | |________POLICE___BOX__________| |
+  |  |============================|  |
+  |  | .-----------..-----------. |  |
+  |  | |  _  _  _  ||  _  _  _  | |  |
+  |  | | | || || | || | || || | | |  |
+  |  | | |_||_||_| || |_||_||_| | |  |
+  |  | | | || || | || | || || | | |  |
+  |  | | |_||_||_| || |_||_||_| | |  |
+  |  | |  _______  ||  _______  | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |_______| || |_______| | |  |
+  |  | |  _______ @||@ _______  | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |_______| || |_______| | |  |
+  |  | |  _______  ||  _______  | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |       | || |       | | |  |
+  |  | | |_______| || |_______| | |  |
+  |  | '-----------''-----------' |  |
+ _|__|/__________________________\\|__|_ 
 '----'----------------------------'----'
 *vworp vworp*
 A TARDIS has materialised!
-Do you want to play a game? (yes())`)
+Do you want to play a game?\nOptions: yes()`)
 
 function yes(){
-    console.log("What do you want to do next? (explore() / exit())")
+    steps += 1
+    info = "What do you want to do next?\nOptions: explore() / exit()"
+    return info
 }
 
 function explore(){
+    steps +=1
     switch(currentRoom){
         case("outside"):
-            console.log("You open the door. It's dark inside, but there's a faint light in the middle.")
-            console.log("What do you want to do next? (closer())")
+            info = "You open the door. It's dark inside, but there's a faint light in the middle.\nWhat do you want to do next?\nOptions: closer()"
             currentRoom = "consoleRoom"
             break;
         case("TARDISMain"):
-            console.log("You cautiously approach one of the entrances in the room. As you enter, the lights turn on.")
-            console.log("What do you want to do next? (explore())")
+            info = "You cautiously approach one of the entrances in the room. As you enter, the lights turn on.\nWhat do you want to do next?\nOptions: explore()"
             currentRoom = "TARDISHallway"
             break;
         case("TARDISHallway"):
-            console.log("The lights pulse gently as you pass through the entrance of the hallway. You can see two doors to your right and left.")
-            console.log("What do you want to do next? try the door on the (left() / right() / exit())")
+            info = "The lights pulse gently as you pass through the entrance of the hallway. You can see two doors to your right and left.\nWhat do you want to do next? try the door on the:\nOptions: left() / right() / exit()"
             break;
         case("TARDISLibrary"):
-            console.log("You enter the room.")
+            libraryVisited += 1
+            info = "You enter the room."
             if (!(inventory.includes("keycard"))){
-                console.log("There's a keycard on the middle table. You put it in your pocket.")
+                info += "\nThere's a keycard on the middle table. You put it in your pocket.\n"
                 inventory.push("keycard")
             }
-            console.log("It's quite dark. The shadows make you feel uneasy.")
-            console.log("What do you want to do next? exit()")
-
-
+            if (libraryVisited == 2){
+                info += "\nHey, who turned out the lights?\nYou try to back away from the shadows, but by then, it's too late."
+                currentRoom = "gameOver";
+                return info + `\nEnding: Bad\nSteps: ${steps}`
+            }
+            info += "\nIt's quite dark. The shadows make you feel uneasy.\nWhat do you want to do next?\nOptions: exit()"
     }
+    return info
 }
 
 function left(){
+    steps +=1
     switch(currentRoom){
         case("TARDISHallway"):
-            console.log("It's locked.")
-            explore()
-            break;
+            info = "It's locked."
+            return info + "\n" + explore();
     }
+    return info;
 }
 
 function right(){
+    steps += 1
     switch(currentRoom){
         case("TARDISHallway"):
-            console.log("The door slides open. It looks like a ... library?")
-            console.log("What do you want to do next? (explore() / exit())")
+            info = "The door slides open. It looks like a ... library?\nWhat do you want to do next?\nOptions: explore() / exit()"
             currentRoom = "TARDISLibrary"
     }
+    return info
 }
 
-function exit(){
+function exit(msg){
+    steps += 1
     switch(currentRoom){
         case("outside"):
-            console.log("You look away from the TARDIS. When you look back, it's gone.")
-            currentRoom = "gameOver"
+            info = `You look away from the TARDIS. When you look back, it's gone.`
+            msg ? info += msg : info += `\nEnding: Neutral\nSteps: ${steps}`
+            currentRoom = "gameOver";
             break;
         case("TARDISHallway"):
+            info = "You return to the main room."
             currentRoom = "consoleRoom"
-            closer()
-            break;
+            return info + "\n" + closer()
         case("TARDISLibrary"):
+            info = "With a gentle hiss, the library doors slide closed behind you."
             currentRoom = "TARDISHallway"
-            explore()
-            break;
-}
-}
+            return info + "\n" + explore()
+        case("TARDISMain"):
+            info = "You decide to look around more before interacting with the console."
+            currentRoom = "consoleRoom"
+            return info + "\n" + closer()
+    }
+    return info;
+    }
 
 function closer(){
+    steps += 1
     switch(currentRoom){
         case("consoleRoom"):
-            console.log("As you approach the console, the lights flash.")
-            console.log("What do you want to do next? (explore()) / inspect())")
+            info = "As you approach the console, the lights flash.\nWhat do you want to do next?\nOptions: explore() / inspect()"
             currentRoom = "TARDISMain"
             break;
     }
+    return info;
 }
 
 function inspect(){
+    steps += 1
     switch(currentRoom){
         case("TARDISMain"):
-            console.log("You look carefully at the controls in front of you. Two things stand out: a lever and a button.")
-            console.log("What do you want to do next? use (lever()) / button())")
+            info = "You look carefully at the controls in front of you. Two things stand out: a lever and a button.\nWhat do you want to do next? use:\nOptions: lever() / button() / exit()"
             break;
             }
+    return info;
 }
 
 function button(){
+    steps += 1
     switch(currentRoom){
         case("TARDISMain"):
             if (!(inventory.includes("keycard"))){
-                console.log("You try to press the button, but nothing happens.")
-                console.log("A screen nearby flashes with a message: PERMISSION DENIED.")
+                info = "You try to press the button, but nothing happens.\nA screen nearby flashes with a message: PERMISSION DENIED."
                 currentRoom = "consoleRoom"
-                closer()
-                break;
+                return info + "\n" + closer();
 
             } else {
-                console.log("You press the button and hear a clicking noise.")
-                console.log("The front door swings open! Grateful to be free, you walk through.")
+                info = `You press the button and hear a clicking noise.\nThe front door swings open! Grateful to be free, you walk through.`
                 currentRoom = "outside"
-                exit()
-                break;
+                return info + "\n"  +exit(`\nEnding: Good\nSteps: ${steps}`);
             }
     }
+    return info;
 }
 
 function lever(){
+    steps+=1
     switch(currentRoom){
         case("TARDISMain"):
-            console.log("You pull the lever. There's an ominious hissing noise, and then everything goes black.")
+            info = `You pull the lever. There's an ominious hissing noise, and then everything goes black.\nEnding: Bad\nSteps: ${steps}`
             currentRoom = "gameOver"
             break;
-
     }
+    return info;
 }
